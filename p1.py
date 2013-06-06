32d9dac5464b029db76a79d7d284bafa7dc9d113
Jesse Huang <jeshuang@eden.rutgers.edu>
2013-06-05 23:10:09 -0400

    success!

---
+++ p1.py
@@ -0,0 +1,20 @@
+s = "FourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentanewnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecreatedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+
+def isPalindrome(s):
+    return s == s[::-1]
+
+maxL = 0
+maxS = ""
+for i in xrange(len(s)):
+    off = 0
+    substring = s[i-off:i+off+1]
+    while(isPalindrome(substring)):
+        off += 1
+        substring = s[i-off:i+off+1]
+    off -= 1
+    substring = s[i-off:i+off+1]
+    if maxL < len(substring):
+        maxL = len(substring)
+        maxS = substring
+
+print maxS
