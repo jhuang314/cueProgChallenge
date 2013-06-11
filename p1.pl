aab972bd01230e169ad22ff990cf9aa3a70a501e
Jesse Huang <jeshuang@eden.rutgers.edu>
2013-06-10 23:19:14 -0400

    finished freaking perl implementation

--- p1.pl
+++ p1.pl
@@ -19,5 +19,30 @@ sub isPalindrone
 my $maxL = 0;
 my $maxS = "";
 
+sub substr
+{
+    my($what, $where, $howmuch) = @_;
+    unpack("x$where a$howmuch", $what);
+}
 
-print "\n";
+for ($i = 0; $i < length($s); $i++)
+{
+    my $off = 0;
+    my $str = "";
+    
+    while (isPalindrone $str) {
+	$str = substr $s, $i-$off, 2 * $off + 1;
+	$off++;
+    }
+    
+    # Got to sub by 2
+    $off -= 2;
+    
+    $str = substr $s, $i-$off, 2 * $off + 1;
+    
+    if ($off >= $maxL) {
+	$maxL = $off;
+	$maxS = $str;
+    }
+}
+print $maxS."\n";
